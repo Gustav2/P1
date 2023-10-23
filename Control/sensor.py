e9f0c5fe002df14a513bf84c15ea3e2af050dfcc
Gustav <gustav@gustavnybro.com>
2023-10-23 18:24:29 +0200

    Added sensor class

---
+++ Control/sensor.py
@@ -0,0 +1,8 @@
+class Sensor:
+    """
+    :param uid_topic: Unique identifier of the sensor for the complete topic
+
+    """
+
+    def __init__(self, uid_topic):
+        self.uid_topic = uid_topic
