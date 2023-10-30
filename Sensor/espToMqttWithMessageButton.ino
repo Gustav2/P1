8a8a165f746d66840b62099fe87c431a182513fb
Viktor-Kirk-Almann-Hansen <144124128+Viktor-Kirk-Almann-Hansen@users.noreply.github.com>
2023-10-30 09:01:40 +0100

    Add files via upload

---
+++ Sensor/espToMqttWithMessageButton.ino
@@ -0,0 +1,106 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+int BUTTON1_PIN = 2; //button is connected to GPIO pin D1
+int BUTTON2_PIN = 3;
+// Update these with values suitable for your network.
+const char* ssid = "P1";//put your wifi ssid here
+const char* password = "password1234";//put your wifi password here.
+const char* mqtt_server = "test.mosquitto.org";
+//const char* mqtt_server = "iot.eclipse.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+
+void setup_wifi() {
+   delay(100);
+  // We start by connecting to a WiFi network
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+      delay(500);
+      Serial.print(".");
+    }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  
+} //end callback
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) 
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    //if you MQTT broker has clientID,username and password
+    //please change following line to    if (client.connect(clientId,userName,passWord))
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+     //once connected to MQTT broker, subscribe command if any
+      client.subscribe("OsoyooCommand");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+} //end reconnect()
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  pinMode(BUTTON1_PIN,INPUT);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  long now = millis();
+  int status;
+  //send message every 2 second
+  if (now - lastMsg > 2000) {
+     lastMsg = now;
+     status=digitalRead(BUTTON1_PIN);
+     String msg="Button status: ";
+     if(status==LOW )
+     {
+        msg= msg+ "Pressed";
+       char message[58];
+       msg.toCharArray(message,58);
+       Serial.println(message);
+       //publish sensor data to MQTT broker
+      client.publish("esp32test", message);
+       }
+     else
+     {
+      msg= msg+ " Not Pressed";
+       char message[58];
+       msg.toCharArray(message,58);
+       Serial.println(message);
+       //publish sensor data to MQTT broker
+      client.publish("esp32test", message);
+     }
+    }
+     
+}
