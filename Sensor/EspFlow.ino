cf8b617819f4d80c7e5cd6c1982801885d5e7e26
Viktor-Kirk-Almann-Hansen <144124128+Viktor-Kirk-Almann-Hansen@users.noreply.github.com>
2023-11-13 14:00:49 +0100

    Add files via upload

---
+++ Sensor/EspFlow.ino
@@ -0,0 +1,150 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <string>
+
+#define SENSOR  6
+
+const char* ssid = "P1";//put your wifi ssid here
+const char* password = "password1234";//put your wifi password here.
+const char* mqtt_server = "192.168.1.149";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+
+long currentMillis = 0;
+long previousMillis = 0;
+int interval = 1000;
+float calibrationFactor = 4.5;
+volatile byte pulseCount;
+byte pulse1Sec = 0;
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+
+void setup_wifi() {
+   delay(100);
+  // We start by connecting to a WiFi network
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+      delay(500);
+      Serial.print(".");
+    }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  
+} //end callback
+
+void IRAM_ATTR pulseCounter()
+{
+  pulseCount++;
+}
+
+void setup()
+{
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  Serial.begin(115200);
+  while (!Serial) {
+      delay(100);
+  }
+  pinMode(SENSOR, INPUT_PULLUP);
+
+  pulseCount = 0;
+  flowRate = 0.0;
+  flowMilliLitres = 0;
+  totalMilliLitres = 0;
+  previousMillis = 0;
+
+  attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) 
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    //if you MQTT broker has clientID,username and password
+    //please change following line to    if (client.connect(clientId,userName,passWord))
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+     //once connected to MQTT broker, subscribe command if any
+      client.subscribe("OsoyooCommand");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+} //end reconnect()
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+    delay(2000);
+    currentMillis = millis();
+    if (currentMillis - previousMillis > interval) {
+      
+      pulse1Sec = pulseCount;
+      pulseCount = 0;
+
+      // Because this loop may not complete in exactly 1 second intervals we calculate
+      // the number of milliseconds that have passed since the last execution and use
+      // that to scale the output. We also apply the calibrationFactor to scale the output
+      // based on the number of pulses per second per units of measure (litres/minute in
+      // this case) coming from the sensor.
+      flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+      previousMillis = millis();
+
+      // Divide the flow rate in litres/minute by 60 to determine how many litres have
+      // passed through the sensor in this 1 second interval, then multiply by 1000 to
+      // convert to millilitres.
+      flowMilliLitres = (flowRate / 60) * 1000;
+
+      // Add the millilitres passed in this second to the cumulative total
+      totalMilliLitres += flowMilliLitres;
+      
+      // Print the flow rate for this second in litres / minute
+      Serial.print("Flow rate: ");
+      Serial.print(int(flowRate));  // Print the integer part of the variable
+      Serial.print("L/min");
+      Serial.print("\t");       // Print tab space
+
+      // Print the cumulative total of litres flowed since starting
+      Serial.print("Output Liquid Quantity: ");
+      Serial.print(totalMilliLitres);
+      Serial.print("mL / ");
+      Serial.print(totalMilliLitres / 1000);
+      Serial.println("L");
+      
+      char msg_out1[2];
+      dtostrf(flowRate,0,2,msg_out1);
+      client.publish("sensor/flowRate", msg_out1);
+      char msg_out2[2];
+      dtostrf(totalMilliLitres,0,2,msg_out2);
+      client.publish("sensor/flowTotal", msg_out2);
+  }
+}
