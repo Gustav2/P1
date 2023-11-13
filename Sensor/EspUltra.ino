eb02d91878ae5c8082a93a7d864f95abb6b46f2a
Viktor-Kirk-Almann-Hansen <144124128+Viktor-Kirk-Almann-Hansen@users.noreply.github.com>
2023-11-13 15:58:29 +0100

    Update EspUltra.ino

--- Sensor/EspUltra.ino
+++ Sensor/EspUltra.ino
@@ -1,6 +1,10 @@
 #include <Arduino.h>
 #include <Ultrasonic.h>
 
+//Vcc wire 5v
+//Gnd wire ground
+//Echo wire on pin 8
+//Trig wire on pin 9
 Ultrasonic ultrasonic1(9, 8);	// An ultrasonic sensor HC-04
 
 
