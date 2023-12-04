d88159b2f317efa2c324521c6762d392a1017ed0
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2023-12-04 09:24:01 +0100

    Add files via upload
    removed rc code and added sleep

--- Sensor/Test/EspTempHumidity.ino
+++ Sensor/Test/EspTempHumidity.ino
@@ -21,73 +21,18 @@ PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 
-void setup_wifi() {
-   delay(100);
-  // We start by connecting to a WiFi network
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-      delay(500);
-      Serial.print(".");
-    }
-  randomSeed(micros());
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+void setup_wifi() { //Connect to wifi. While loop breaks when connected.
+  WiFi.begin(ssid, password); 
+  while (WiFi.status() != WL_CONNECTED);
 }
-
-void callback(char* topic, byte* payload, unsigned int length) 
-{
-  
-} //end callback
 
 void setup() {
+    esp_sleep_enable_timer_wakeup(60000000*30); // 60000000==1 min 
     setup_wifi();
     client.setServer(mqtt_server, 1883);
-    client.setCallback(callback);
-    Serial.begin(115200);
-    while (!Serial) {
-        delay(100);
-    }
     Wire.begin();
     sht4x.begin(Wire);
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) 
-  {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    //if you MQTT broker has clientID,username and password
-    //please change following line to    if (client.connect(clientId,userName,passWord))
-    if (client.connect(clientId.c_str()))
-    {
-      Serial.println("connected");
-     //once connected to MQTT broker, subscribe command if any
-      client.subscribe("OsoyooCommand");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-} //end reconnect()
-
-void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-    delay(2000);
+    client.connect("ESP32Temp/Hum");
     float temperature;
     float humidity;
     sht4x.measureHighPrecision(temperature, humidity);
@@ -95,8 +40,15 @@ void loop() {
         char msg_out1[2];
         dtostrf(temperature,0,2,msg_out1);
         client.publish("sensor/temp", msg_out1);
+        
         char msg_out2[2];
         dtostrf(humidity,0,2,msg_out2);
         client.publish("sensor/humidity", msg_out2);
     }
+    delay(60);
+    esp_deep_sleep_start();
+}
+
+void loop() {
+  
 }
