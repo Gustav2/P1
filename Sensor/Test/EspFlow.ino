0d5b0be20a65681f2abedaca9f8591e408f4e2e0
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2023-12-04 09:31:50 +0100

    removed rc from code

--- Sensor/Test/EspFlow.ino
+++ Sensor/Test/EspFlow.ino
@@ -27,28 +27,11 @@ float flowRate;
 unsigned int flowMilliLitres;
 unsigned long totalMilliLitres;
 
-void setup_wifi() {
-   delay(100);
-  // We start by connecting to a WiFi network
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-      delay(500);
-      Serial.print(".");
-    }
-  randomSeed(micros());
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+void setup_wifi() { //Connect to wifi. While loop breaks when connected.
+  WiFi.begin(ssid, password); 
+  while (WiFi.status() != WL_CONNECTED);
 }
 
-void callback(char* topic, byte* payload, unsigned int length) 
-{
-  
-} //end callback
 
 void IRAM_ATTR pulseCounter()
 {
@@ -59,11 +42,7 @@ void setup()
 {
   setup_wifi();
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  Serial.begin(115200);
-  while (!Serial) {
-      delay(100);
-  }
+  client.connect("ESP32Flow");
   pinMode(SENSOR, INPUT_PULLUP);
 
   pulseCount = 0;
@@ -73,39 +52,11 @@ void setup()
   previousMillis = 0;
 
   attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
-}
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) 
-  {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    //if you MQTT broker has clientID,username and password
-    //please change following line to    if (client.connect(clientId,userName,passWord))
-    if (client.connect(clientId.c_str()))
-    {
-      Serial.println("connected");
-     //once connected to MQTT broker, subscribe command if any
-      client.subscribe("OsoyooCommand");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-} //end reconnect()
+}
 
 void loop()
 {
-  if (!client.connected()) {
-    reconnect();
-  }
   client.loop();
     delay(2000);
     currentMillis = millis();
