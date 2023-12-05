5d5dc94e4fec970e491428e34d28c5f93dd69c22
mholms23 <mholms23@student.aau.dk>
2023-12-05 11:01:22 +0100

    fixed docstrings and serial connect

--- Sensor/Test/laser.ino
+++ Sensor/Test/laser.ino
@@ -8,7 +8,9 @@ const char* mqtt_server = "192.168.1.149"; //mqtt server ip
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-//grøn = ground - hvid ved ldr = 0 - blå = 3v - hvid ved laser = 5 - orange = ground
+// ved resistor: grøn = ground, hvid = 0, brun = 5v 
+// ved laser: orange = ground, hvid = 5v
+
 int ldr = 0; //analog pin to which LDR is connected
 int ldr_value = 0; //variable to store LDR values
 int old_ldr = 0;
@@ -19,7 +21,6 @@ void setup_wifi() { //Connect to wifi. While loop breaks when connected.
 }
 
 void setup() {
-  Serial.begin(9600); //start the serial monitor
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.connect("ESP32Laser");
