225e7e5c19931b902825189bb57c80e4aa787882
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2023-12-05 13:26:05 +0100

    removed rc

--- Sensor/Test/EspFlow.ino
+++ Sensor/Test/EspFlow.ino
@@ -11,11 +11,13 @@
 const char* ssid = "P1";//put your wifi ssid here
 const char* password = "password1234";//put your wifi password here.
 const char* mqtt_server = "192.168.1.149";
+const char* ntpServer = "1.dk.pool.ntp.org"; //NTP server
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
+
+RTC_DATA_ATTR unsigned int time_to_midnight;
+int one_sec  = 1000000; //micro second to 1 one second
 
 long currentMillis = 0;
 long previousMillis = 0;
@@ -32,6 +34,14 @@ void setup_wifi() { //Connect to wifi. While loop breaks when connected.
   while (WiFi.status() != WL_CONNECTED);
 }
 
+// Function that gets current epoch time
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  getLocalTime(&timeinfo);
+  time(&now);
+  return now;
+}
 
 void IRAM_ATTR pulseCounter()
 {
@@ -40,6 +50,7 @@ void IRAM_ATTR pulseCounter()
 
 void setup()
 {
+  Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.connect("ESP32Flow");
@@ -53,10 +64,18 @@ void setup()
 
   attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
 
+  configTime(0, 0, ntpServer);
+  time_to_midnight = 86400 - (getTime() % 86400);
 }
 
 void loop()
-{
+{ int status;
+  time_to_midnight = 86400 - (getTime() % 86400);
+  Serial.println(time_to_midnight);
+  if (time_to_midnight == 0) {
+    client.publish("sensor/alive", "ESP32Flow");
+  }
+
   client.loop();
     delay(2000);
     currentMillis = millis();
@@ -64,41 +83,21 @@ void loop()
       
       pulse1Sec = pulseCount;
       pulseCount = 0;
-
-      // Because this loop may not complete in exactly 1 second intervals we calculate
-      // the number of milliseconds that have passed since the last execution and use
-      // that to scale the output. We also apply the calibrationFactor to scale the output
-      // based on the number of pulses per second per units of measure (litres/minute in
-      // this case) coming from the sensor.
       flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
       previousMillis = millis();
-
-      // Divide the flow rate in litres/minute by 60 to determine how many litres have
-      // passed through the sensor in this 1 second interval, then multiply by 1000 to
-      // convert to millilitres.
       flowMilliLitres = (flowRate / 60) * 1000;
 
-      // Add the millilitres passed in this second to the cumulative total
       totalMilliLitres += flowMilliLitres;
-      
-      // Print the flow rate for this second in litres / minute
-      Serial.print("Flow rate: ");
-      Serial.print(int(flowRate));  // Print the integer part of the variable
-      Serial.print("L/min");
-      Serial.print("\t");       // Print tab space
-
-      // Print the cumulative total of litres flowed since starting
-      Serial.print("Output Liquid Quantity: ");
-      Serial.print(totalMilliLitres);
-      Serial.print("mL / ");
-      Serial.print(totalMilliLitres / 1000);
-      Serial.println("L");
-      
+      if (flowRate > 0) { 
       char msg_out1[2];
       dtostrf(flowRate,0,2,msg_out1);
       client.publish("sensor/flowRate", msg_out1);
       char msg_out2[2];
       dtostrf(totalMilliLitres,0,2,msg_out2);
       client.publish("sensor/flowTotal", msg_out2);
+      delay(50);
+      } 
+      
   }
+
 }
