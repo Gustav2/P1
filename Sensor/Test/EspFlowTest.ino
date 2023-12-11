375d08a634838a8dff8163b4e3894d9645102dfb
Viktor-Kirk-Almann-Hansen <144124128+Viktor-Kirk-Almann-Hansen@users.noreply.github.com>
2023-12-11 11:11:34 +0100

    Update EspFlowTest.ino
    added comments

--- Sensor/Test/EspFlowTest.ino
+++ Sensor/Test/EspFlowTest.ino
@@ -42,8 +42,17 @@ void loop() {
       
       pulse1Sec = pulseCount;
       pulseCount = 0;
-      flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+      // Because this loop may not complete in exactly 1 second intervals we calculate
+      // the number of milliseconds that have passed since the last execution and use
+      // that to scale the output. We also apply the calibrationFactor to scale the output
+      // based on the number of pulses per second per units of measure (litres/minute in
+      // this case) coming from the sensor.
+      flowRate = ((1000.0 / (millis() – previousMillis)) * pulse1Sec) / calibrationFactor;
       previousMillis = millis();
+     
+      // Divide the flow rate in litres/minute by 60 to determine how many litres have
+      // passed through the sensor in this 1 second interval, then multiply by 1000 to
+      // convert to millilitres.
       flowMilliLitres = (flowRate / 60) * 1000;
 
       totalMilliLitres += flowMilliLitres;
