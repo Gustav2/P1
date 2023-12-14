6c6ff630ff018e1729c3dd70f6d19cb922086578
Viktor-Kirk-Almann-Hansen <144124128+Viktor-Kirk-Almann-Hansen@users.noreply.github.com>
2023-12-14 11:11:04 +0100

    Update LDR_sensor.ino
    Made it so it reconnects to wifi, mqtt and ntp if something disconnects

--- Sensor/Final Product/LDR_sensor.ino
+++ Sensor/Final Product/LDR_sensor.ino
@@ -8,51 +8,71 @@
 
 int LDR_Pin = 2;         // LDR og 10K pulldown er forbundet til 2
 int LDR_Reading;         // Analog læsning fra LDR
-int LDR_Threshold = 100; // Lys grænse for åbning af køleskab
+int LDR_Threshold = 1400; // Lys grænse for åbning af køleskab
 int Previous_State = -1; // Initialiser den forrige tilstand
 
 RTC_DATA_ATTR unsigned int time_to_midnight;
 int one_sec  = 1000000; //micro second to 1 one second
 
-const char* UID = "ESP32LDR"; //UID
 const char* ssid = "P1"; //wifi ssid
 const char* password = "password1234"; //wifi password
 const char* mqtt_server = "192.168.1.149"; //mqtt server ip
 const char* ntpServer = "1.dk.pool.ntp.org"; //NTP server
+const char* UID = "ESP32LDR"; //UID
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+// Function that gets current epoch time
+unsigned long getTime() {
+  time_t now;
+  struct tm timeinfo;
+  getLocalTime(&timeinfo);    
+  time(&now);
+  return now;
+}
 
 void setup_wifi() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED);
 }
 
-// Function that gets current epoch time
-unsigned long getTime() {
-  time_t now;
-  struct tm timeinfo;
-  getLocalTime(&timeinfo);
-  time(&now);
-  return now;
+void setup_mqtt() {
+  client.setServer(mqtt_server, 1883);
+  client.connect(UID); //set the UID.
+  while (!client.connected());
 }
 
-void setup() {
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.connect("x"); //set the UID.
+void setup_ntp() {  
   configTime(0, 0, ntpServer);
+  delay(50);
+}
+
+void initialize() {
+  setup_wifi();
+  setup_mqtt();
+  setup_ntp();
+}
+
+void setup() {    
+  initialize();
 }
 
 void loop() {
-  delay(50);
+  unsigned long getTime();
+  unsigned long now;
+  if (WiFi.status() != WL_CONNECTED || !client.connected() || !(now = getTime())) {
+    initialize();
+    delay(50);
+  }
+
+  delay(150);
   LDR_Reading = analogRead(LDR_Pin);
   time_to_midnight = 86400 - (getTime() % 86400);
-
+  delay(50);
   if (time_to_midnight == 0) {
     client.publish("sensor/alive", UID);
   }
-
   if (LDR_Reading >= LDR_Threshold && Previous_State != 0) {
     client.publish("sensor/LDR", "Fridge_Open");
     Previous_State = 0;
